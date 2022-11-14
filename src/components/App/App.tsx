2465f06ca012ce629b0026fd4fd1b04294f05036
weldone-dev <roma.kalita2015@gmail.com>
2022-11-15 00:15:11 +0300

    add support people with low vision

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,11 @@
 import React, {useEffect} from "react";
 import {useNavigate, useParams} from "react-router-dom";
 import {
-	countCol,
-	generateColorForURL,
-	generateRandomColor,
-	isInCorrectnessCheck,
-	parseColorFromParam
+    countCol,
+    generateColorForURL,
+    generateRandomColor,
+    isInCorrectnessCheck,
+    parseColorFromParam
 } from "../../utils";
 
 import styles from "./App.module.scss";
@@ -13,62 +13,62 @@ import ControlButton from "../ControlButton/ControlButton";
 import ColumnBlock from "../ColumnBlock/ColumnBlock";
 
 const App = () => {
-		const {colorId} = useParams()
+    const {colorId} = useParams()
 
-		const navigate = useNavigate()
-		const initColors = React.useMemo(() => {
-				if (colorId) return parseColorFromParam(colorId)
-				return Array.from({length: countCol}, generateRandomColor)
-		}, [colorId])
-		const [isLocks, setIsLocks] = React.useState(Array.from({length: countCol}, _ => false))
-		const [colors, setColors] = React.useState(initColors)
-		React.useEffect(() => {
-				if (!colorId || isInCorrectnessCheck(colorId)) {
-						navigate(generateColorForURL(colors))
-				}
-		}, [colorId, colors, navigate])
-		const handleReloadColor = React.useCallback(() => {
-				setColors(
-					colors => {
-							const newColor = colors.map(
-								(value, index) => isLocks[index]
-									? value
-									: generateRandomColor()
-							)
-							navigate(generateColorForURL(newColor))
-							return newColor
-					}
-				)
-		}, [isLocks, navigate])
-		useEffect(() => {
-				const handlerKeyDownEvent = (event: KeyboardEvent) => {
-						if (event.code.toLowerCase() === 'space') {
-								event.preventDefault()
-								handleReloadColor()
-						}
-				}
-				window.addEventListener("keydown", handlerKeyDownEvent)
-				return () => {
-						window.removeEventListener("keydown", handlerKeyDownEvent)
-				}
-		}, [handleReloadColor, isLocks])
+    const navigate = useNavigate()
+    const initColors = React.useMemo(() => {
+        if (colorId) return parseColorFromParam(colorId)
+        return Array.from({length: countCol}, generateRandomColor)
+    }, [colorId])
+    const [isLocks, setIsLocks] = React.useState(Array.from({length: countCol}, _ => false))
+    const [colors, setColors] = React.useState(initColors)
+    React.useEffect(() => {
+        if (!colorId || isInCorrectnessCheck(colorId)) {
+            navigate(generateColorForURL(colors))
+        }
+    }, [colorId, colors, navigate])
+    const handleReloadColor = React.useCallback(() => {
+        setColors(
+            colors => {
+                const newColor = colors.map(
+                    (value, index) => isLocks[index]
+                        ? value
+                        : generateRandomColor()
+                )
+                navigate(generateColorForURL(newColor))
+                return newColor
+            }
+        )
+    }, [isLocks, navigate])
+    useEffect(() => {
+        const handlerKeyDownEvent = (event: KeyboardEvent) => {
+            if (event.code.toLowerCase() === 'space') {
+                event.preventDefault()
+                handleReloadColor()
+            }
+        }
+        window.addEventListener("keydown", handlerKeyDownEvent)
+        return () => {
+            window.removeEventListener("keydown", handlerKeyDownEvent)
+        }
+    }, [handleReloadColor, isLocks])
 
-		const handleClickLock = React.useCallback((index: number, value: boolean) => {
-				setIsLocks(isLocks => isLocks.map((content, i) => i === index ? value : content))
-		}, [])
-		return (
-			<div className={styles.wrapper}>
-					{isLocks.map((lock, index) =>
-						<ColumnBlock
-							key={index}
-							index={index}
-							isLock={lock}
-							color={colors[index]}
-							onClickLock={handleClickLock}
-						/>
-					)}
-					<ControlButton handlerReload={handleReloadColor}/>
-			</div>
-		)
+    const handleClickLock = React.useCallback((index: number, value: boolean) => {
+        setIsLocks(isLocks => isLocks.map((content, i) => i === index ? value : content))
+    }, [])
+    return (
+        <div className={styles.wrapper}>
+            {isLocks.map((lock, index) =>
+                <ColumnBlock
+                    key={index}
+                    index={index}
+                    isLock={lock}
+                    color={colors[index]}
+                    onClickLock={handleClickLock}
+                />
+            )}
+            <ControlButton handlerReload={handleReloadColor}/>
+        </div>
+    )
 }
 export default App
--- src/components/ControlButton/ControlButton.tsx
+++ src/components/ControlButton/ControlButton.tsx
@@ -4,30 +4,31 @@ import styles from './ControlButton.module.scss'
 import {copyToClickBoard} from "../../utils";
 
 interface Props {
-		handlerReload(): void
+    handlerReload(): void
 }
 
 const handleShareClick = async () => {
-		if (navigator.share) {
-				await navigator.share({
-						title: 'Colors',
-						text: 'Take a look at this color!',
-						url: window.location.href,
-				})
-		} else {
-				await copyToClickBoard(window.location.href)
-		}
+    if (navigator.share) {
+        await navigator.share({
+            title: 'Colors',
+            text: 'Take a look at this color!',
+            url: window.location.href,
+        })
+    }
+    else {
+        await copyToClickBoard(window.location.href)
+    }
 }
 const ControlButton = ({handlerReload}: Props) => {
-		return (
-			<div className={styles.wrapper}>
-					<div className={styles.reload} onClick={handlerReload}>
-							<FontAwesomeIcon icon={["fas", "rotate"]}/>
-					</div>
-					<button className={styles.share} onClick={handleShareClick}>
-							<FontAwesomeIcon icon={["fas", "share"]}/>
-					</button>
-			</div>
-		);
+    return (
+        <div className={styles.wrapper}>
+            <div className={styles.reload} onClick={handlerReload}>
+                <FontAwesomeIcon icon={["fas", "rotate"]}/>
+            </div>
+            <button aria-label={"share"} className={styles.share} onClick={handleShareClick}>
+                <FontAwesomeIcon icon={["fas", "share"]}/>
+            </button>
+        </div>
+    );
 }
 export default React.memo(ControlButton)
