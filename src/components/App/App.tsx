46ab053fab54813a5e73769ad67e020105218c95
weldone-dev <roma.kalita2015@gmail.com>
2022-11-17 18:58:20 +0300

    Optimization and fix height in mobile browsers

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -12,34 +12,42 @@ import styles from "./App.module.scss";
 import ControlButton from "../ControlButton/ControlButton";
 import ColumnBlock from "../ColumnBlock/ColumnBlock";
 
+
 const App = () => {
     const {colorId} = useParams()
 
     const navigate = useNavigate()
-    const initColors = React.useMemo(() => {
-        if (colorId) return parseColorFromParam(colorId)
-        return Array.from({length: countCol}, generateRandomColor)
-    }, [colorId])
+
     const [isLocks, setIsLocks] = React.useState(Array.from({length: countCol}, _ => false))
-    const [colors, setColors] = React.useState(initColors)
+    const [colors, setColors] = React.useState(colorId
+        ? parseColorFromParam(colorId)
+        : Array.from({length: countCol}, generateRandomColor)
+    )
+    React.useEffect(() => {
+        const handleResize = () => {
+            const vh = window.innerHeight * 0.01;
+            document.documentElement.style.setProperty('--vh', `${vh}px`);
+        }
+        handleResize()
+        window.addEventListener('resize', handleResize);
+        return window.removeEventListener('resize', handleResize)
+    }, [])
     React.useEffect(() => {
         if (!colorId || isInCorrectnessCheck(colorId)) {
             navigate(generateColorForURL(colors))
         }
     }, [colorId, colors, navigate])
-    const handleReloadColor = React.useCallback(() => {
-        setColors(
-            colors => {
-                const newColor = colors.map(
-                    (value, index) => isLocks[index]
-                        ? value
-                        : generateRandomColor()
-                )
-                navigate(generateColorForURL(newColor))
-                return newColor
-            }
-        )
-    }, [isLocks, navigate])
+    const handleReloadColor = React.useCallback(() => setColors(
+        colors => {
+            const newColor = colors.map(
+                (value, index) => isLocks[index]
+                    ? value
+                    : generateRandomColor()
+            )
+            navigate(generateColorForURL(newColor))
+            return newColor
+        }
+    ), [isLocks, navigate])
     useEffect(() => {
         const handlerKeyDownEvent = (event: KeyboardEvent) => {
             if (event.code.toLowerCase() === 'space') {
@@ -52,10 +60,10 @@ const App = () => {
             window.removeEventListener("keydown", handlerKeyDownEvent)
         }
     }, [handleReloadColor, isLocks])
-
-    const handleClickLock = React.useCallback((index: number, value: boolean) => {
-        setIsLocks(isLocks => isLocks.map((content, i) => i === index ? value : content))
-    }, [])
+    const handleClickLock = React.useCallback((index: number, value: boolean) =>
+        setIsLocks(isLocks => isLocks.map((content, i) => i === index ? value : content)),
+        []
+    )
     return (
         <div className={styles.wrapper}>
             {isLocks.map((lock, index) =>
